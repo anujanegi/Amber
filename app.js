f7577fcf5ba1cad36da7a4d6a94f666483acc985
anujanegi <anujanegi1@gmail.com>
2019-06-02 00:58:06 +0530

    Boilerplate setup

---
+++ app.js
@@ -0,0 +1,28 @@
+const request = require('request');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var indexRouter = require('./routes/index');
+var ambulance = require('./routes/ambulance');
+var user = require('./routes/user');
+
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// API endpoints
+app.use('/user', user);
+// app.use('/ambulance', ambulance);
+module.exports = app;
+app.listen(8080);
